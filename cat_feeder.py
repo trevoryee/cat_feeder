2f1f037bfeaf3005f2b84976bf965cbcdf689348
trevoryee <trevor_yee@icloud.com>
2020-12-15 09:00:16 -0600

    Update cat_feeder.py

--- cat_feeder.py
+++ cat_feeder.py
@@ -193,7 +193,33 @@ def SetAngle(angle)
 	GPIO.output(servoPIN, False)
 	p.ChangeDutyCycle(0)
 
-SetAngle(90)
-SetAngle(-90)
-pwm.stop()
-GPIO.cleanup()
+
+
+#actual function
+def cat_feed:
+	get_weight= food_weight
+	if food_weight <= 500
+	full= false
+	while full = false:
+		SetAngle(90)
+		sleep(5)
+		SetAngle(-90)
+		sleep(5)
+		SetAngle(90)
+		sleep(5)
+		pwm.stop()
+		GPIO.cleanup()
+		print ("The food weight is now" + get_weight)
+		full= true
+		sleep (10800)
+		if get_weight <= 500:
+			full= false
+	else:
+		print ("The dish is full")
+		full= true
+		sleep(10800)
+		if get_weight <= 500
+			full= false
+tare
+cat_feed
+	
