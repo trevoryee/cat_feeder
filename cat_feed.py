916b1186aeb271095529e2d8e9d4a38c43470705
trevoryee <trevor_yee@icloud.com>
2020-12-15 09:10:30 -0600

    Update cat_feed.py

--- cat_feed.py
+++ cat_feed.py
@@ -14,15 +14,14 @@ def cat_feed:
 		pwm.stop()
 		GPIO.cleanup()
 		print ("The food weight is now" + get_weight)
-		full= true
 		sleep (10800)
 		if get_weight <= 500:
 			full= false
-	else:
-		print ("The dish is full")
-		full= true
-		sleep(10800)
-		if get_weight <= 500
-			full= false
+		if else:
+			print ("The dish is full")
+			full= true
+			sleep(10800)
+			if get_weight <= 500
+				full= false
 tare
 cat_feed
